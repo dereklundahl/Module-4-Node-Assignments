eed2a50a4a631ed7495cccb4149dc36a8ad214ae
Derek Lundahl <dereklundahl@yahoo.com>
2018-09-13 15:17:52 -0600

    added the .js files for the assignment

---
+++ goodbye.js
@@ -0,0 +1,7 @@
+exports.goodbyeEnglish = function() {
+  console.log("Later skater");
+}
+
+exports.goodbyeSpanish = function() {
+  console.log("Adios muchachos");
+}
---
+++ main.js
@@ -0,0 +1,14 @@
+const greeting = require("./greeting.js");
+const goodbye = require("./goodbye.js");
+
+greeting.greetingEnglish();
+
+greeting.greetingJapanese();
+
+greeting.greetingSpanish();
+
+greeting.greetingIcelandic();
+
+goodbye.goodbyeEnglish();
+
+goodbye.goodbyeSpanish();
